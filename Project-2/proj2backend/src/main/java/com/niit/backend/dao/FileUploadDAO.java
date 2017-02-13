9d41553303d72ffd0fbc531eaf8146e4a168f022
venkatesulunaidu <mandalavenkatesh70@gmail.com>
2017-02-13 10:35:18 +0530

    proj2

--- Project-2/proj2backend/src/main/java/com/niit/backend/dao/FileUploadDAO.java
+++
@@ -1,8 +0,0 @@
-package com.niit.backend.dao;
-
-import com.niit.backend.model.UploadFile;
-
-public interface FileUploadDAO {
-	void save(UploadFile uploadFile);
-	UploadFile getFile(String username);
-}
